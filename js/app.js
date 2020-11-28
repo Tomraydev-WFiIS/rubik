72cf0d4bd2be37dc4de053db01ee6e23dab4076c
tomray <tomraydev@gmail.com>
2020-11-28 22:49:26 +0100

    fix linter errors

--- js/app.js
+++ js/app.js
@@ -1,30 +1,37 @@
-import * as THREE from 'https://threejs.org/build/three.module.js';
-import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
-// import { Stats } from 'https://mrdoob.github.io/stats.js/build/stats.module.js';
+"use strict";
+import * as THREE from "https://threejs.org/build/three.module.js";
+import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
+// import { Stats } from "https://mrdoob.github.io/stats.js/build/stats.module.js";
 
-let camera, scene, renderer, controls, axesHelper;
-let geometry, material;
+let camera;
+let scene;
+let renderer;
+let controls;
+let axesHelper;
+let geometry;
+let material;
 let stopwatch;
 
-var debug = false;
-var cubes = [];
-var colors = [
+
+const debug = false;
+let cubes = [];
+const colors = [
     0xffffff, //white
     0xffd600, //yellow
     0x0044af, //blue
     0x009c46, //green
     0xff5700, //orange
-    0xb80a31, //red
+    0xb80a31 //red
 ];
-var spacing = 1.02; // 1.02
-var moveQueue = [];
-var userMoveHistory = [];
-var shuffleMoveHistory = [];
-var moveInProgress = false;
-var gameInProgress = false;
-var startingGame = false;
-var defaultSpeed = 0.05; // 0.05
-var speed = defaultSpeed;
+let spacing = 1.02; // 1.02
+let moveQueue = [];
+let userMoveHistory = [];
+let shuffleMoveHistory = [];
+let moveInProgress = false;
+let gameInProgress = false;
+let startingGame = false;
+const defaultSpeed = 0.05; // 0.05
+let speed = defaultSpeed;
 
 
 init();
@@ -41,7 +48,6 @@ function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xf5f5f5);
     if (debug) scene.add(axesHelper);
-
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.x = 5;
     camera.position.y = 5;
@@ -72,11 +78,11 @@ function createObjects() {
     for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
             for (let k = -1; k <= 1; k++) {
-                var cube = newCube();
+                let cube = newCube();
                 cube.position.x = i*spacing;
                 cube.position.y = j*spacing;
                 cube.position.z = k*spacing;
-                cube.initialPosition = cube.position.clone()
+                cube.initialPosition = cube.position.clone();
 
                 if (i == -1){
                     cube.geometry.faces[1*2].color.setHex(colors[0]);
@@ -103,7 +109,7 @@ function createObjects() {
                     cube.geometry.faces[4*2 +1].color.setHex(colors[5]);
                 }
                 scene.add(cube);
-                cubes.push(cube)
+                cubes.push(cube);
             }
         }
     }
@@ -111,7 +117,7 @@ function createObjects() {
 
 function newCube() {
     geometry = new THREE.BoxGeometry(1,1,1);
-    for (var i = 0; i < geometry.faces.length; i++) {
+    for (let i = 0; i < geometry.faces.length; i++) {
         geometry.faces[i].color.setHex(0x000000);
     }
     material = new THREE.MeshBasicMaterial({vertexColors: true});
@@ -119,16 +125,17 @@ function newCube() {
 }
 
 function getActiveGroup(axis, orientation) {
+    let position;
     switch(orientation) {
         case "+":
-            var position = spacing;
+            position = spacing;
             break;
         case "-":
-            var position = -spacing;
+            position = -spacing;
             break;
     }
-    var activeGroup = [];
-    for(var cube of cubes){
+    let activeGroup = [];
+    for(let cube of cubes){
         if (cube.position[axis] == position) {
             activeGroup.push(cube);
         }
@@ -137,25 +144,25 @@ function getActiveGroup(axis, orientation) {
 }
 
 function turnFace(move) {
-    var axis = move.axis;
-    var orientation = move.orientation;
-    var direction = move.direction;
+    let axis = move.axis;
+    let orientation = move.orientation;
+    let direction = move.direction;
 
-    var activeGroup = getActiveGroup(axis, orientation);
-    var pivot = new THREE.Object3D();
+    let activeGroup = getActiveGroup(axis, orientation);
+    let pivot = new THREE.Object3D();
     pivot.rotation.set(0,0,0);
     pivot.updateMatrixWorld(); //needed?
     scene.add(pivot);
-    for(var cube of activeGroup){
+    for(let cube of activeGroup){
         pivot.attach(cube);
     }
-    var step = Math.PI * speed;
+    let step = Math.PI * speed;
     function animateTurn(agg){
         if ((agg + step) >= Math.PI/2.0) {
             pivot.rotation[axis] = direction * Math.PI/2.0;
             pivot.updateMatrixWorld(); // needed?
-            for(var cube of activeGroup){
-                scene.attach(cube)
+            for(let cube of activeGroup){
+                scene.attach(cube);
                 cube.position.x = Math.round((cube.position.x + Number.EPSILON) * 100) / 100;
                 cube.position.y = Math.round((cube.position.y + Number.EPSILON) * 100) / 100;
                 cube.position.z = Math.round((cube.position.z + Number.EPSILON) * 100) / 100;
@@ -173,10 +180,10 @@ function turnFace(move) {
 }
 document.addEventListener("keydown", onDocumentKeyDown);
 function onDocumentKeyDown(event) {
-    var keyCode = event.which || event.keyCode;
+    let keyCode = event.which || event.keyCode;
     if (keyCode == 16) return; // shift
-    var direction = event.shiftKey?1:-1;
-    var move;
+    let direction = event.shiftKey?1:-1;
+    let move;
     switch(keyCode) {
         case 88: // x - down
             move = {axis: "y", orientation: "-", direction: direction};
@@ -208,14 +215,14 @@ function applyMoves() {
         moveInProgress = true;
         turnFace(moveQueue.shift());
     }
-    if (moveQueue.length == 0 && !moveInProgress && startingGame) {
+    if (moveQueue.length === 0 && !moveInProgress && startingGame) {
         startGame2();
     }
 }
 
 function shuffle(turns=20) {
-    var previous_move = getRandomMove();
-    for(var i = 0; i < turns;){
+    let previous_move = getRandomMove();
+    for(let i = 0; i < turns;){
         let move = getRandomMove();
         if (areOppositeMoves(move, previous_move)) continue;
         moveQueue.push(move);
@@ -226,7 +233,7 @@ function shuffle(turns=20) {
 }
 
 function getRandomMove() {
-    return {axis: choose(["x", "y", "z"]), orientation: choose(["+", "-"]), direction: choose([1, -1])}
+    return {axis: choose(["x", "y", "z"]), orientation: choose(["+", "-"]), direction: choose([1, -1])};
 }
 
 function areOppositeMoves(m1, m2) {
@@ -242,7 +249,7 @@ function resetSpeed(){
 }
 
 function choose(choices) {
-    var index = Math.floor(Math.random() * choices.length);
+    let index = Math.floor(Math.random() * choices.length);
     return choices[index];
 }
 
@@ -273,26 +280,26 @@ function finishGame() {
 
 function startStopwatch() {
     document.getElementById("stopwatch").innerHTML = "00:00:00";
-    var start = new Date().getTime();
+    let start = new Date().getTime();
 
     stopwatch = setInterval(function() {
-        var elapsed =  new Date(new Date().getTime() - start);
-        var hh = pad(elapsed.getUTCHours());
-        var mm = pad(elapsed.getMinutes());
-        var ss = pad(elapsed.getSeconds());
-        var timeString = `${hh}:${mm}:${ss}`;
+        let elapsed =  new Date(new Date().getTime() - start);
+        let hh = pad(elapsed.getUTCHours());
+        let mm = pad(elapsed.getMinutes());
+        let ss = pad(elapsed.getSeconds());
+        let timeString = `${hh}:${mm}:${ss}`;
         document.getElementById("stopwatch").innerHTML = timeString;
     }, 1000);
 }
 
-function pad(n){return n<10 ? '0'+n : n}
+function pad(n){return n<10 ? "0"+n : n}
 
 function stopStopwatch() {
     clearInterval(stopwatch);
 }
 
 function resetGame() {
-    for(var cube of cubes){
+    for(let cube of cubes){
         scene.remove(cube);
     }
     cubes = [];
@@ -313,29 +320,29 @@ function checkCube(cube){
         cube.position.x == cube.initialPosition.x &&
         cube.position.y == cube.initialPosition.y &&
         cube.position.z == cube.initialPosition.z
-    )
+    );
 }
 
 function solve() {
     // primitive solver, reverse every move made
     let moves = shuffleMoveHistory.concat(userMoveHistory).reverse();
-    for(var move of moves){
+    for(let move of moves){
         move.direction = (move.direction==1)?-1:1;
         moveQueue.push(move);
     }
 }
 
 function createStats() {
-    var stats = new Stats();
+    let stats = new Stats();
     stats.setMode(0);
 
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = '0';
-    stats.domElement.style.top = '0';
+    stats.domElement.style.position = "absolute";
+    stats.domElement.style.left = "0";
+    stats.domElement.style.top = "0";
 
     return stats;
 }
 
 // exports
-window.startGame = startGame
-window.solve = solve
+window.startGame = startGame;
+window.solve = solve;
